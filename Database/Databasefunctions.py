b0289565b700a7ad1b27ee2e0ffddd49529393e2
minh <vu2.tran@student.uni-siegen.de>
2020-03-30 00:08:30 +0200

    changed data types

--- Database/Databasefunctions.py
+++ Database/Databasefunctions.py
@@ -1,11 +1,13 @@
 import _sqlite3
-from .passwordHasher import hash_password,verify_password
+from .passwordHasher import hash_password, verify_password
 from .SessionIDGenerator import generate_session
+
 conn = _sqlite3.connect('shopBaseWithExample.db')
 
 c = conn.cursor()
 
-def registerUser(last_name, first_name, postcode, street, house_number, password,email):
+
+def registerUser(last_name, first_name, postcode, street, house_number, password, email):
     """regestier a user into database hash password !! and return true when success else false"""
     sql = """ INSERT INTO user (email, last_name, first_name,postcode, street, house_number, password) VALUES ('{}','{}','{}',{},'{}','{}','{}')"""
     hashedPw = hash_password(password)
@@ -15,11 +17,12 @@ def registerUser(last_name, first_name, postcode, street, house_number, password
             sqlFor = sql.format(email, last_name, first_name, postcode, street, house_number, hashedPw)
             print(sqlFor)
             c.execute(sqlFor)
-    except:
+    except Exception as registerUserException:
+        print('getUserFromSessionException: ', registerUserException)
         success = False
 
-
     return success
+
 
 def getUserfromSessionID(sessionID):
     """ a function to help find the user_id from the sessionID folder"""
@@ -31,10 +34,12 @@ def getUserfromSessionID(sessionID):
             print(sqlFor)
             c.execute(sqlFor)
             result = c.fetchone()[0]
-    except:
+    except Exception as getUserFromSessionException:
+        print('getUserFromSessionException: ', getUserFromSessionException)
         result = -1
 
     return result
+
 
 def login(email, password):
     """ check if user is in database if in db generate sesid and insert into db  return sessid if success else -1"""
@@ -47,43 +52,84 @@ def login(email, password):
             print(sqlF)
             c.execute(sqlF)
             userInfo = c.fetchone()
-    except:
-        print("no such email")
+    except Exception as loginException:
+        print('loginException: ', loginException)
         return -1
-    if verify_password(userInfo[1],password):
+
+    if verify_password(userInfo[1], password):
         sessionID = generate_session()
+
         try:
             with conn:
                 sql = """ INSERT INTO session VALUES ("{}", {} )"""
                 sqlF = sql.format(sessionID, userInfo[2])
                 print(sqlF)
                 c.execute(sqlF)
-        except:
+
+        except Exception as verifyPasswordException:
+            print('verifyPasswordException: ', verifyPasswordException)
             return -1
+
     else:
         return -1
     return sessionID
 
+
 def logout(sessionID):
     """ remove sesID from db return true if success else false"""
-    pass
+
+    sql = """ DELETE FROM session WHERE session_id = '{}'"""
+    try:
+        with conn:
+            sqlLogout = sql.format(sessionID)
+            print(sqlLogout)
+            c.execute(sqlLogout)
+            print("Logout Successful!")
+
+
+    except Exception as LogoutException:
+        print('LogoutException: ', LogoutException)
+
 
 def getArtikels():
     """return all articels as a List with dictonary {item_id:, name:}"""
-    pass
+    sqlGetArtikels = """ SELECT item_id, item_name FROM item """
+    try:
+        with conn:
+            print(sqlGetArtikels)
+            c.execute(sqlGetArtikels)
+            artikelList = c.fetchall()
 
-def getAuftraege(sessionId,plz):
+            artikelz = []
+            if artikelList is not None:
+                for ass in artikelList:
+                    artikelz.append({"item_id": ass[0], "item_name": ass[1]})
+
+                print(artikelz)
+                return list
+
+            else:
+                return -1
+
+    except Exception as getArtikelsException:
+        print('getArtikelsException: ', getArtikelsException)
+
+
+def getAuftraege(sessionId, plz):
     """check if plz is -1 if -1 return all auftraege assoziated with that user else all auftrege in that plz  return type: list with dictonary {errandid, postcode} else -1"""
     pass
 
-def getAuftrag(sessionid,auftragid):
+
+def getAuftrag(sessionid, auftragid):
     """get all the information from that errand as a dictonary"""
     pass
+
 
 def takeErrand(sessionID, errandID):
     """in db add to user_accepted_errand if errend isnt taken (status = 0), change status from errand to one  return true or false """
     pass
 
-def makeErrand(postcode,email, phone_number, notice, articleList):
+
+def makeErrand(postcode, email, phone_number, notice, articleList):
     """ create an new errand with the information status default is 0 / articleList looks like that [{item_id, amount}] add to errand and the from article list in items_in_errand  return bool for success"""
     pass
--- testing.py
+++ testing.py
@@ -1,7 +1,10 @@
 import Database
 
-
 print(Database.registerUser("test_last", "test_vor", "57072", "street.str", "16a", "testing", "test@test.de"))
 
 ses = Database.login("test@test.de", "testing")
 print(ses)
+
+Database.logout(ses)
+
+Database.getArtikels()
